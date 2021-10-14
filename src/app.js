a0a0e50b5a959a65a3944fad42b93fd20a85c5cc
Mahsa Irankhah <mahsairankhah77@gmail.com>
2021-10-14 21:20:50 +0330

    Added relevent icon from API

--- src/app.js
+++ src/app.js
@@ -22,22 +22,26 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 function displayTempreture(response) {
-  console.log(response.data);
   let tempretureElement = document.querySelector("#tempreture");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   tempretureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apikey = "a710bd8bd76400c9658ef649d9e81728";
-let city = "tehran";
+let city = "new york";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
 axios.get(apiUrl).then(displayTempreture);
